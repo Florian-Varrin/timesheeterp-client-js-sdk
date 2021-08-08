4f40d93caf539cfb12230a8875b4ecc1b3fb3bf5
fvarrin <florian.varrin@gmail.com>
2021-08-08 15:29:54 +0200

    Create auth and user services

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,5 @@
+export type AuthCredentialsDto = {
+  email: string;
+
+  password: string;
+}
---
+++ src/auth/users/user.service.ts
@@ -0,0 +1,67 @@
+import AbstractResourceService from '../../abstract/abstract.resource.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import {UserEntity} from './entity/user.entity';
+import {UpdateUserDto} from './dto/update-user.dto';
+
+class UserService extends AbstractResourceService {
+  protected resourceUrl: string;
+
+  constructor(
+    protected apiUrl: string,
+    protected accessToken: string | undefined
+  ) {
+    super(apiUrl, accessToken);
+
+    this.resourceUrl = `${apiUrl}/users`;
+  }
+
+  async findOneById(userId: number): Promise<UserEntity> {
+    try {
+      const { data: user } = await this.makeRequest('GET', `${this.resourceUrl}/${userId}`);
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findAll(): Promise<UserEntity[]> {
+    try {
+      const { data: user } = await this.makeRequest('GET', this.resourceUrl);
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    try {
+      const { data: user } = await this.makeRequest('POST', this.resourceUrl, createUserDto);
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(userId: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    try {
+      const { data: user } = await this.makeRequest('PATCH', `${this.resourceUrl}/${userId}`, updateUserDto);
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async remove(userId: number): Promise<void> {
+    try {
+      await this.makeRequest('DELETE', `${this.resourceUrl}/${userId}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default UserService;
